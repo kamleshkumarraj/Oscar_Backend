314c557fb10ecfda860196cfcb94a77b27a16c94
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-11-05 21:24:56 +0530

    We successfully added a attribute in users model for role

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role : {type : String , default : 'user'},
   insertTime : {type: Number, required : false}
 });
 
